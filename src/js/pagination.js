b3b9b350c485b77690a6663a21d2196b82fc8775
DariiaNikolaieva <nikolaieva.dariya@gmail.com>
2021-06-19 00:02:05 +0300

    вапрь

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,6 +1,7 @@
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.min.css';
 import getRefs from './get-refs';
+import movie from '../templates/movie-card.hbs'
 
 const refs = getRefs();
 
@@ -18,8 +19,8 @@ export default function createPagination(totalItems, itemsPerPage, visiblePages,
 const { visiblePages } = setItemsPerPage();
 createPagination(20000, 20, visiblePages, onPagination);
 
-export default function onPagination(eventData) {
-  const url = `https://api.themoviedb.org/3/trending/movie/day?api_key=bf08c0c07642287cbabe383c02818eb3&page=${eventData.page}`;
+export default function onPagination(event) {
+  const url = `https://api.themoviedb.org/3/trending/movie/day?api_key=c27b75f2098a52933ae8847a9b55ad4e&page=${event.page}`;
 
   movieForHomePage(url);
 }
@@ -45,3 +46,44 @@ export default function setItemsPerPage(movieData = []) {
   }
   return { itemsPerPage, visiblePages, newMovieData };
 }
+
+function movieForHomePage(url) {
+    // const spinner = new Spinner(opts).spin(refs.targetSpinner);
+  getMovies(url)
+    .then(data => data.results)
+    .then(movies => {
+      refs.moviesList.innerHTML = '';
+    //   changeGenre(movies); // змінює жанр
+    //   changeData(movies); // змінює дату
+
+      // залежно від ширини viewport залишає необхідну кількість елементів
+      const { newMovieData } = setItemsPerPage(movies);
+      renderMarkup(newMovieData, movie, refs.moviesList); // створення розмітки по шаблону
+    //   spinner.stop(); // зупиняє спінер
+    });
+}
+
+export default async function getMovies(URL, page = 1) {
+  try {
+    const responsee = await fetch(URL);
+    const data = await responsee.json();
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+function renderMarkup(data, template, domElementLink) {
+  const markup = template(data);
+  domElementLink.insertAdjacentHTML('beforeend', markup);
+}
+
+export { renderMarkup, searchResultsMarkup };
+
+function mainPageRender() {
+  const url = `https://api.themoviedb.org/3/trending/movie/day?api_key=c27b75f2098a52933ae8847a9b55ad4e`;
+  movieForHomePage(url);
+}
+
+mainPageRender();
