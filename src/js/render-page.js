d6c2f8e130985a89c794df266ad418e7a2b0af16
Halyna Sinko <galinasinko14@gmail.com>
2021-06-20 17:58:57 +0300

    Update render-page.js

--- src/js/render-page.js
+++ src/js/render-page.js
@@ -2,8 +2,8 @@ import getRefs from './get-refs';
 import searchFieldTpl from '../templates/search-field.hbs';
 import headerBtnsTpl from '../templates/header-btns.hbs'
 
-console.log(searchFieldTpl());
-console.log(headerBtnsTpl());
+// console.log(searchFieldTpl());
+// console.log(headerBtnsTpl());
 
 const refs = getRefs();
 
@@ -12,19 +12,33 @@ refs.home.addEventListener('click', onHomeClick);
 refs.myLibrary.addEventListener('click', onMyLibraryClick);
 
 insertSearchField();
-console.log(refs.home);
-console.log(refs.myLibrary);
-console.log(refs.headerDynamicContainer);
+// console.log(refs.home);
+// console.log(refs.myLibrary);
+// console.log(refs.headerDynamicContainer);
 
 
-function onHomeClick(e) {
-    console.log(e.target);
+function onHomeClick(event) {
+    // console.log(event.target);
+    const activClass=event.target.classList.contains("site-nav__button--active");
+    if(activClass){
+      
+      refs.home.classList.remove("site-nav__button--active");
+    } else {
+       event.target.classList.toggle("site-nav__button--active");
+       refs.home.classList.toggle("site-nav__button--active");
+    }
     clearContainer();
     insertSearchField();
 }
 
-function onMyLibraryClick(e) {
-  console.log(e.target);
+function onMyLibraryClick(event) {
+  // console.log(event.target);
+  const activClass=event.target.classList.contains("site-nav__button--active");
+  // console.log(activClass);
+   if(!activClass){
+      event.target.classList.toggle("site-nav__button--active");
+      refs.myLibrary.classList.toggle("site-nav__button--active");
+    }
   clearContainer();
   insertHeaderBtns();
 }
