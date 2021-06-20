94dd563e55a839cfabf36b08fdc68755994b1525
Halyna Sinko <galinasinko14@gmail.com>
2021-06-20 23:46:37 +0300

    fixed toggle class

--- src/js/render-page.js
+++ src/js/render-page.js
@@ -11,7 +11,7 @@ const refs = getRefs();
 refs.home.addEventListener('click', onHomeClick);
 refs.myLibrary.addEventListener('click', onMyLibraryClick);
 
-insertSearchField();
+insertContent(refs.headerDynamicContainer, searchFieldTpl);
 // console.log(refs.home);
 // console.log(refs.myLibrary);
 // console.log(refs.headerDynamicContainer);
@@ -19,39 +19,32 @@ insertSearchField();
 
 function onHomeClick(event) {
     // console.log(event.target);
-    const activClass=event.target.classList.contains("site-nav__button--active");
-    if(activClass){
-      
-      refs.home.classList.remove("site-nav__button--active");
-    } else {
-       event.target.classList.toggle("site-nav__button--active");
-       refs.home.classList.toggle("site-nav__button--active");
-    }
-    clearContainer();
-    insertSearchField();
+  const activClass = event.target.classList.contains("site-nav__button--active");
+  if(!activClass){
+    event.target.classList.add("site-nav__button--active");
+  }
+  refs.myLibrary.classList.remove('site-nav__button--active');
+  clearContainer(refs.headerDynamicContainer);
+  insertContent(refs.headerDynamicContainer, searchFieldTpl);
 }
 
 function onMyLibraryClick(event) {
   // console.log(event.target);
   const activClass=event.target.classList.contains("site-nav__button--active");
   // console.log(activClass);
-   if(!activClass){
-      event.target.classList.toggle("site-nav__button--active");
-      refs.myLibrary.classList.toggle("site-nav__button--active");
-    }
-  clearContainer();
-  insertHeaderBtns();
+  if(!activClass){
+      event.target.classList.add("site-nav__button--active");
+  }
+  refs.home.classList.remove('site-nav__button--active');
+  clearContainer(refs.headerDynamicContainer);
+  insertContent(refs.headerDynamicContainer, headerBtnsTpl);
 }
 
-function insertSearchField() {
-    refs.headerDynamicContainer.insertAdjacentHTML('beforeend', searchFieldTpl());
+function insertContent(nameContainer,fnTemplates) {
+  nameContainer.insertAdjacentHTML('beforeend', fnTemplates());
 }
 
-function insertHeaderBtns() {
-  refs.headerDynamicContainer.insertAdjacentHTML('beforeend', headerBtnsTpl());
-}
-
-function clearContainer(){
- refs.headerDynamicContainer.innerHTML=''
+function clearContainer(nameContainer) {
+  nameContainer.innerHTML = '';
 }
 
