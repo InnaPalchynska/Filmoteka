91118c938444eff7236b9cb1eab31cb2e4392cfc
Halyna Sinko <galinasinko14@gmail.com>
2021-06-20 16:58:01 +0300

    Create render-page.js

---
+++ src/js/render-page.js
@@ -0,0 +1,43 @@
+import getRefs from './get-refs';
+import searchFieldTpl from '../templates/search-field.hbs';
+import headerBtnsTpl from '../templates/header-btns.hbs'
+
+console.log(searchFieldTpl());
+console.log(headerBtnsTpl());
+
+const refs = getRefs();
+
+
+refs.home.addEventListener('click', onHomeClick);
+refs.myLibrary.addEventListener('click', onMyLibraryClick);
+
+insertSearchField();
+ 
+console.log(refs.myLirary);
+console.log(refs.headerDynamicContainer);
+
+
+function onHomeClick(e) {
+    console.log(e.target);
+    clearContainer();
+    insertSearchField();
+}
+
+function onMyLibraryClick(e) {
+  console.log(e.target);
+  clearContainer();
+  insertHeaderBtns();
+}
+
+function insertSearchField() {
+    refs.headerDynamicContainer.insertAdjacentHTML('beforeend', searchFieldTpl());
+}
+
+function insertHeaderBtns() {
+  refs.headerDynamicContainer.insertAdjacentHTML('beforeend', headerBtnsTpl());
+}
+
+function clearContainer(){
+ refs.headerDynamicContainer.innerHTML=''
+}
+
