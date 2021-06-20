3381948fee1d64688470526f5e80c31a9ef20db5
Halyna Sinko <galinasinko14@gmail.com>
2021-06-20 14:04:48 +0300

    add refs

--- src/js/get-refs.js
+++ src/js/get-refs.js
@@ -3,5 +3,8 @@ export default function getRefs() {
     moviesList: document.querySelector('.js-movies-list'),
     divPagination: document.querySelector('.tui-pagination'),
     preloader: document.querySelector('.js-preloader'),
+    home: document.querySelector('[data-header="home"]'),
+    myLibrary: document.querySelector('[data-header="home"]'),
+    headerDynamicContainer: document.querySelector('[data-header="dynamicContainer"]'),
   };
 }
