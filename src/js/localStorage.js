4b864c817d2e079334f0433d2202504325e4de28
prodanserega <74560554+prodanserega@users.noreply.github.com>
2021-06-21 17:57:15 +0300

    1

---
+++ src/js/localStorage.js
@@ -0,0 +1,34 @@
+const modalButtonWatched = document.querySelector('.js-modal-btn-watched');
+const modalButtonQueue = document.querySelector('.js-modal-btn-queue');
+
+modalButtonWatched.addEventListener('click', onWatchedButton);
+modalButtonQueue.addEventListener('click', onQueueButton);
+
+if (!localStorage.getItem('filmWatched')) {
+  localStorage.setItem('filmWatched', JSON.stringify([]));
+}
+if (localStorage.getItem('filmWatched')) {
+  const filmsStorage = localStorage.getItem('filmWatched');
+  const filmsStorageArray = JSON.parse(filmsStorage);
+  if (filmsStorageArray.includes(modalButtonWatched.id)) {
+    modalButtonWatched.textContent = 'Remove from watched';
+    modalButtonWatched.classList.add('active');
+  }
+}
+
+function onWatchedButton(event) {
+  const filmsStorage = localStorage.getItem('filmWatched');
+  const filmsStorageArray = JSON.parse(filmsStorage);
+  if (!filmsStorageArray.includes(event.target.id)) {
+    filmsStorageArray.push(event.target.id);
+    event.target.textContent = 'REMOVE FROM WATCHED';
+    event.target.classList.add('active');
+    localStorage.setItem('filmWatched', JSON.stringify(filmsStorageArray));
+  } else {
+    event.target.textContent = 'ADD TO WATCHED';
+    event.target.classList.remove('active');
+    const index = filmsStorageArray.indexOf(event.target.id);
+    filmsStorageArray.splice(index, 1);
+    localStorage.setItem('filmWatched', JSON.stringify(filmsStorageArray));
+  }
+}
