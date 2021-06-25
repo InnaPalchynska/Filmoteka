7700195053c4a18dbfe3d0e85a19c50334a78b50
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-06-25 08:31:49 +0300

    adds js for creating and opening footer lightbox

---
+++ src/js/footer-lightbox.js
@@ -0,0 +1,51 @@
+import getRefs from '../js/get-refs';
+import footerLightboxTpl from '../templates/footer-lightbox.hbs';
+
+import '../sass/components/_basic-lightbox.scss';
+import * as basicLightbox from 'basiclightbox';
+
+const refs = getRefs();
+refs.footerBtn.addEventListener('click', onFooterBtnClick);
+
+function onFooterBtnClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== 'BUTTON') {
+    return;
+  }
+
+  onLightboxOpen(e);
+}
+
+function onLightboxOpen(e) {
+  const lightbox = basicLightbox.create(footerLightboxTpl(), {
+    onShow() {
+      refs.body.classList.add('inactive');
+    },
+
+    onClose() {
+      refs.body.classList.remove('inactive');
+    },
+  });
+
+  lightbox.show();
+
+  const closeBtn = document.querySelector('.footer-lightbox__close-btn');
+
+  closeBtn.addEventListener('click', onLightboxClose);
+  window.addEventListener('keydown', onEscBtnPress);
+
+  function onLightboxClose(e) {
+    lightbox.close();
+
+    closeBtn.removeEventListener('click', onLightboxClose);
+  }
+
+  function onEscBtnPress(e) {
+    if (e.code === 'Escape') {
+      lightbox.close();
+    }
+
+    window.removeEventListener('keydown', onEscBtnPress);
+  }
+}
