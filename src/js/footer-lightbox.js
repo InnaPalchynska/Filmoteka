936401cee426d270689b5c5346973e172b297467
Inna Palchynska <InnaPalchynska@gmail.com>
2021-06-28 22:51:46 +0300

    update

--- src/js/footer-lightbox.js
+++ src/js/footer-lightbox.js
@@ -1,11 +1,13 @@
 import getRefs from '../js/get-refs';
+import teamMembers from './team';
 import footerLightboxTpl from '../templates/footer-lightbox.hbs';
 
-import 'basiclightbox/src/styles/main.scss';
+import '../sass/components/_basic-lightbox.scss';
 import * as basicLightbox from 'basiclightbox';
 
 const refs = getRefs();
 refs.footerBtn.addEventListener('click', onFooterBtnClick);
+console.log(teamMembers);
 
 function onFooterBtnClick(e) {
   e.preventDefault();
@@ -18,7 +20,7 @@ function onFooterBtnClick(e) {
 }
 
 function onLightboxOpen(e) {
-  const lightbox = basicLightbox.create(footerLightboxTpl(), {
+  const lightbox = basicLightbox.create(footerLightboxTpl(teamMembers), {
     onShow() {
       refs.body.classList.add('inactive');
     },
@@ -29,17 +31,6 @@ function onLightboxOpen(e) {
   });
 
   lightbox.show();
-
-  const photo1 = document.querySelector('[data-photo="photo--Inna"]');
-  const photo2 = document.querySelector('[data-photo="photo--Anastasia"]');
-  const photo3 = document.querySelector('[data-photo="photo--Halyna"]');
-  const photo4 = document.querySelector('[data-photo="photo--Dariia"]');
-  const photo5 = document.querySelector('[data-photo="photo--Kateryna"]');
-  const photo6 = document.querySelector('[data-photo="photo--Sergiy"]');
-
-  console.log(photo1);
-  photo1.src = '../images/team/team__2__mob.jpg';
-  console.log(photo1.src);
 
   const closeBtn = document.querySelector('.footer-lightbox__close-btn');
 
